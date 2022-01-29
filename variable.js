7f1c3b4b370f4d0620447a15c9f886737c6aa6ab
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-29 19:04:00 +0600

    first commit video 18-1 and 18-2

---
+++ variable.js
@@ -0,0 +1,12 @@
+// Numeric variable
+var watchPrice = 120; 
+watchPrice = 120; 
+
+// String
+var userName = 'Sogir Uddin';
+
+// Boolean 
+var isExpensive = true;
+
+// Integer 1 2 3 4 5 6 145 
+// Float 2.714 5.98745 3.14
