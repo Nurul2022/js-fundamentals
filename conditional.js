89a6051881bc1d4588348175c316a65a6d6b5b15
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-29 22:41:40 +0600

    conditional added

---
+++ conditional.js
@@ -0,0 +1,32 @@
+var isFoodReady = false;
+// jodi food is ready hoi taile ami khamu 
+// if food is ready i will eat 
+//if food is ready == true
+// if(isFoodReady == true){
+//     console.log('Mama!! i will eat now')
+// }
+// var iphonePrice = 70000;
+// var myBudget = 9500;
+
+// //if(iphonePrice < myBudget){
+// //    console.log('iphone dia pic fuani marbo')
+// //}
+
+// if(iphonePrice > myBudget){
+//     console.log('My Xiaomi is the best !!');
+// }
+// if (condition){
+//     //
+// }
+// else{
+
+// }
+var chickenPrice = 180;
+var mymoney = 850;
+
+if (chickenPrice < mymoney) {
+    console.log('Yes! murgir Ran khamu ar haddi chabamu')
+}
+else {
+    console.log('Smashed potato with Lentil soup')
+}
