0de55dd9ae833fd5d4dd4915fb94554f41e4e983
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-29 19:59:44 +0600

    18-3 added

---
+++ array-index.js
@@ -0,0 +1,15 @@
+var bookPages = ['hablu', 'bolod', 'chg', 'battery', 'para', 'gym'];
+
+var batteryIndex = bookPages.indexOf('battery');
+//console.log(batteryIndex);
+
+var numbers = [3, 7, 10, 65, 82];
+var index = numbers.indexOf(152);
+//console.log(index);
+var books = ['hablu', 'bolod', 'chg', 'battery', 'para', 'gym'];
+var secondIndex = books[2];
+//console.log(secondIndex);
+console.log(numbers);
+numbers[1] = 95;
+numbers[4] = 21;
+console.log(numbers);
