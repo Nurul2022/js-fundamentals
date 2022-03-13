7fd099b1fbee7b44b6ba5d8ae62b241ee164d8e6
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-13 12:48:38 +0600

    nested added

--- comparison.js
+++ comparison.js
@@ -1,17 +1,18 @@
-//console.log(5 > 6);
-//console.log(5 < 6);
-//console.log(5==6);
-//console.log(6==6);
-//console.log(5 !=6);
-//console.log(6 !=6);
-//console.log(6 >=6);
+// Comparison
+// console.log(5 > 6);      // false
+// console.log(5 < 6);     // true
+// console.log(5==6);     // false   
+// console.log(6==6);    // true
+// console.log(5 !=6);  // true
+// console.log(6 !=6); // false 
+// console.log(6 >=6); // true
 
 var myLove = 99;
 var yourLove = 100;
-//console.log(myLove > yourLove);
-//console.log(myLove == yourLove);
-//console.log(myLove < yourLove);
-//console.log(myLove != yourLove);
+// console.log(myLove > yourLove);  // false 
+// console.log(myLove == yourLove);  // false 
+// console.log(myLove < yourLove);  // true
+// console.log(myLove != yourLove);  // true
 
 console.log(studyGood && goodPerson);
 console.log(isBanker || goodResult);
--- nested-condition.js
+++ nested-condition.js
@@ -4,19 +4,20 @@ var toastPrice = 10;
 var myBudget = 7;
 var packedWell = false;
 
-// if(danishPrice < myBudget){
-//     console.log('Danish kheye danish jabo');
-// }
-// else if(butterBreadPrice < myBudget){
-//     console.log('Butter bon diye cha khamu');
-// }
-// else if(toastPrice < myBudget){
-//     console.log('toast biscuit khamu')
-// }
-// else {
-//     console.log('Batasha diya cha khamu')
-// }
+if(danishPrice < myBudget){
+    console.log('Danish kheye danish jabo');
+}
+else if (butterBreadPrice < myBudget){
+    console.log('Butter bon diye cha khamu');
+}
+else if (toastPrice < myBudget){
+    console.log('toast biscuit khamu')
+}
+else {
+    console.log('Batasha diya cha khamu')
+} // rs Batasha diya cha khamu
 
+// Nested Condition
 if(danishPrice < myBudget){
     if(packedWell == true){
         console.log('Danish Khabo')
